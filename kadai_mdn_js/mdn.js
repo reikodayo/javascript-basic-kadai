9e15175f0f32e5d952da9bfdeb93ad8d1c419453
reikodayo <kono1125222@gmail.com>
2025-11-02 15:34:24 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,6 +0,0 @@
-const today = new Date();
-const year = today.getUTCFullYear();
-const month = today.getUTCMonth() +1;
-const day = today.getDate();
-
-console.log(year +'年' + month + '月' + day +'日')
